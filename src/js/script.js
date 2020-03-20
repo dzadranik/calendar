d0f35c030623a79a5ef8941b845b90544d7463f4
Victoria <dzadranik@gmail.com>
2020-03-20 17:44:45 +0300

    update script

--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,58 @@
-const { loadEmployee, loadEventInformation } = Employee(),
+const { loadEmployee, displayEventInformation } = Employee(),
 	{ getYear } = Calendar()
 
-const eventContainer = document.querySelector('.js-vacation-table')
-eventContainer.addEventListener('mouseover', event => {
-	if (
-		event.target.hasAttribute('data-event-id') &&
-		event.target.hasAttribute('data-event-id') !== event.relatedTarget.hasAttribute('data-event-id')
-	)
-		loadEventInformation(event.target.getAttribute('data-event-id'))
-})
-eventContainer.addEventListener('mouseover', event => {
-	if (event.relatedTarget.hasAttribute('data-event-id') && !event.target.hasAttribute('data-event-id')) {
-		document.querySelector('.js-vacation-table .calendar-event').remove()
-	}
-})
+window.addEventListener('load', () => {
+	const eventContainer = document.querySelector('.js-vacation-table')
 
+	eventContainer.addEventListener('mouseover', event => {
+		if (event.target.dataset.eventId && event.target.dataset.eventId !== event.relatedTarget.dataset.eventId)
+			displayEventInformation(event.target.dataset.eventId)
+	})
+	eventContainer.addEventListener('mouseover', event => {
+		//event.relatedTarget проверка чтобы при движении из консоли не вылетала ошибка
+		if (event.relatedTarget && event.relatedTarget.dataset.eventId && !event.target.dataset.eventId) {
+			document.querySelector('.js-vacation-table .calendar-event').remove()
+		}
+	})
+})
 loadEmployee()
 
 function Employee() {
-	let employeeData = []
-
-	const getEmployee = data => {
-		employeeData = data
-		addToContainer(getEmployeeContent(data), '.js-vacation-table')
-		data.forEach(element => addEventsToCalendar(element))
-	}
-
-	const getEmployeeContent = data =>
-		data
-			.map(item => `<tr employee-id="${item.id}"> ${getContentOneEmployee(item) + getYear(item.id)} </tr>`)
-			.join('')
-
-	const getContentOneEmployee = data => {
-		let { name, img } = data
-		return `<td>
-					<img class="vacation__employee-photo" src="${img}" alt="Employee image" />
-					<div class="vacation__employee-name">${name}</div>
-				</td>
-				<td>${data.vacationDaysCount}/28</td>`
-	}
+	let employeesArray = []
 
 	const addToContainer = (content, containerSelector) => {
 		const container = document.querySelector(containerSelector)
 		container.innerHTML += content
 	}
 
-	const addEventsToCalendar = data => {
-		const { id, events } = data
-		events.forEach(event => addOneEventOneEmployee(event, id))
+	const displayEmployees = employees => {
+		employeesArray = employees
+		addToContainer(getEmployeesContent(), '.js-vacation-table')
 	}
 
-	const addOneEventOneEmployee = (data, employeeId) => {
-		let { id, dateStart, dateEnd, name } = data,
+	const getEmployeesContent = () =>
+		employeesArray.map(item => `<tr> ${getContentOneEmployee(item) + getYear(item.id)} </tr>`).join('')
+
+	const getContentOneEmployee = employee => {
+		let { name, img, vacationDaysCount } = employee
+		return `<td>
+					<img class="vacation__employee-photo" src="${img}" alt="Employee image" />
+					<div class="vacation__employee-name">${name}</div>
+				</td>
+				<td>${vacationDaysCount}/28</td>`
+	}
+
+	const displayEmployeesEvents = () => {
+		employeesArray.forEach(employee => addEventsToCalendar(employee))
+	}
+
+	const addEventsToCalendar = employee => {
+		const { id, events } = employee
+		events.forEach(event => addOneEvent(event, id))
+	}
+
+	const addOneEvent = (event, employeeId) => {
+		let { id, name, dateStart, dateEnd } = event,
 			dateStartEvent = dateStart.split('.').map(parseFloat),
 			dateEndEvent = dateEnd.split('.').map(parseFloat),
 			startDay = dateStartEvent[0],
@@ -59,33 +60,32 @@ function Employee() {
 			endDay = dateEndEvent[0],
 			endMonth = dateEndEvent[1]
 
-		const findDayContainer = (day, month) =>
-			document.querySelector(`[data-day="${day}-${month - 1}-${employeeId}"]`)
+		const findDayContainer = (day, month) => document.querySelector(`.js-day-${day}-${month - 1}-${employeeId}`)
 
-		const addEventClassAndEventId = (dayContainer, month) => {
+		const addEventClassAndAttribute = (dayContainer, month) => {
 			dayContainer.classList.add(`calendar__day--${name}`)
 			dayContainer.setAttribute('data-event-id', `${employeeId}-${id}-${month}`)
 		}
 
 		if (startMonth === endMonth) {
 			for (let i = startDay; i <= endDay; i++) {
-				addEventClassAndEventId(findDayContainer(i, startMonth), startMonth - 1)
+				addEventClassAndAttribute(findDayContainer(i, startMonth), startMonth - 1)
 			}
 		} else {
 			for (let i = startDay; i <= 31; i++) {
 				const dayContainer = findDayContainer(i, startMonth)
 
 				if (dayContainer) {
-					addEventClassAndEventId(dayContainer, startMonth - 1)
+					addEventClassAndAttribute(dayContainer, startMonth - 1)
 				}
 			}
 			for (let i = 1; i <= endDay; i++) {
-				addEventClassAndEventId(findDayContainer(i, endMonth), endMonth - 1)
+				addEventClassAndAttribute(findDayContainer(i, endMonth), endMonth - 1)
 			}
 		}
 	}
 
-	const getCalendarEvent = (employeeInformation, eventInformation) => {
+	const getCalendarEventContent = (employeeInformation, eventInformation) => {
 		const { name, img } = employeeInformation,
 			{ name: eventName, dateStart, dateEnd } = eventInformation
 		return `<div class="calendar-event">
@@ -97,24 +97,42 @@ function Employee() {
 						<div class="calendar-event__date calendar-event__date--${eventName}">
 							${dateStart}.2020 — ${dateEnd}.2020 (14д.)
 						</div>
-						<div class="calendar-event__name">отпуск</div>
+						<div class="calendar-event__name">${getEventName(eventName)}</div>
 					</div>
 				</div>`
+	}
+
+	const getEventName = name => {
+		return name === 'vacation'
+			? 'отпуск'
+			: name === 'red'
+			? 'красная метка'
+			: name === 'yellow'
+			? 'желтая метка'
+			: ''
 	}
 
 	return {
 		loadEmployee: () => {
 			fetch('https://dzadranik.github.io/i-dex/src/js/vacation-calendar.json')
 				.then(response => response.json())
-				.then(data => getEmployee(data))
+				.then(employees => {
+					displayEmployees(employees)
+					displayEmployeesEvents()
+				})
 		},
-		loadEventInformation: eventId => {
-			let eventData = eventId.split('-').map(parseFloat)
-			let employeeInformation = employeeData.find(item => item.id === eventData[0]),
-				eventInformation = employeeInformation.events.find(event => event.id === eventData[1])
+		displayEventInformation: eventId => {
+			//eventId[day, month, employeeId]
+			let eventIdArray = eventId.split('-').map(parseFloat),
+				eventDay = eventIdArray[0],
+				eventMonth = eventIdArray[1],
+				eventEmployeeId = eventIdArray[2],
+				employeeInformation = employeesArray.find(item => item.id === eventDay),
+				eventInformation = employeeInformation.events.find(event => event.id === eventMonth)
+
 			addToContainer(
-				getCalendarEvent(employeeInformation, eventInformation),
-				`.js-month-${employeeInformation.id}-${eventData[2]}`
+				getCalendarEventContent(employeeInformation, eventInformation),
+				`.js-month-${employeeInformation.id}-${eventEmployeeId}`
 			)
 		}
 	}
@@ -125,75 +143,79 @@ function Calendar() {
 	// daysInMonth-количество дней в месяце,
 	// firstDay-день недели первого дня,
 	// month-индекс месяца,
-	// employeeId- id сотрудника,
+	// employeeId- id сотрудника для формирования js-month-** класса,
 	// day- счетчик дней
 
-	const returnOneMonth = m => {
-		day = 1
-		setMonthData(m)
+	const setEmployeeId = id => {
+		return (employeeId = id)
+	}
 
-		let monthContain = ''
-		const getWeeks = (i = 0) => {
-			if (day <= daysInMonth) {
-				monthContain += getOneWeek(i)
-				getWeeks(i + 1)
-			}
+	const setMonthInformation = monthIndex => {
+		month = monthIndex
+		daysInMonth = new Date(2020, monthIndex + 1, 0).getDate()
+		let frD = new Date(2020, monthIndex, 1).getDay()
+		frD === 0 ? (firstDay = 6) : (firstDay = frD - 1) //сделать отсчет дней недели с понедельника
+	}
+
+	const getYearContent = (i = 0, content = '') => {
+		if (i < 12) {
+			content += getOneMonth(i)
+			return getYearContent(i + 1, content)
+		} else {
+			return content
 		}
-		getWeeks()
+	}
+
+	const getOneMonth = m => {
+		day = 1
+		setMonthInformation(m)
+		//js-month-{employeeId}-{month} формируется класс для поиска дня => отобразить event
 		return `<td>
-					<div class="calendar__month js-month-${employeeId}-${m}">
-						${monthContain}
+					<div class="calendar__month js-month-${employeeId}-${m}"> 
+						${getWeeks()}
 					</div>
 				</td>`
 	}
 
-	const setMonthData = monthIndex => {
-		month = monthIndex
-		daysInMonth = new Date(2020, monthIndex + 1, 0).getDate()
-		let frD = new Date(2020, monthIndex, 1).getDay()
-		frD === 0 ? (firstDay = 6) : (firstDay = frD - 1)
+	const getWeeks = (i = 0, content = '') => {
+		if (day <= daysInMonth) {
+			content += getOneWeek()
+			return getWeeks(i + 1, content)
+		} else {
+			return content
+		}
 	}
 
-	const getOneWeek = weekNumber => {
-		let daysContent = '',
-			getDays = (firstDay, i = 0) => {
-				if (i < 7) {
-					if ((firstDay > 0 && weekNumber === 0) || day > daysInMonth) {
-						daysContent += getOneDay('calendar__empty')
-					} else {
-						daysContent += getOneDay('calendar__day')
-					}
-
-					getDays(firstDay - 1, i + 1)
-				}
-			}
-		getDays(firstDay)
+	const getOneWeek = () => {
 		return `<div class="calendar__week">
-					${daysContent}
+					${getDays()}
 				</div>`
 	}
 
+	const getDays = (i = 0, daysContent = '') => {
+		if (i < 7) {
+			if ((firstDay > 0 && day < 7) || day > daysInMonth) {
+				daysContent += getOneDay('calendar__empty')
+				firstDay--
+			} else {
+				daysContent += getOneDay('calendar__day')
+			}
+			return getDays(i + 1, daysContent)
+		}
+		return daysContent
+	}
+
 	const getOneDay = dayClass => {
-		let isEmptyDay = dayClass === 'calendar__empty',
-			dayContent = `<div class="${dayClass}" ${
-				isEmptyDay ? '' : `data-day="${day}-${month}-${employeeId}"`
-			}></div>`
-		isEmptyDay ? '' : (day = day + 1)
-		return dayContent
+		let isEmptyDay = dayClass === 'calendar__empty'
+		!isEmptyDay ? day++ : ''
+		//чтобы не вводить лишнюю переменную сначала прибавить день, а в формировании класса его вычесть
+		return `<div class="${dayClass} ${!isEmptyDay ? `js-day-${day - 1}-${month}-${employeeId}` : ``}" ></div>`
 	}
 
 	return {
 		getYear: id => {
-			employeeId = id
-			let yearContent = ''
-			const getYearContent = (i = 0) => {
-				if (i < 12) {
-					yearContent += returnOneMonth(i)
-					getYearContent(i + 1)
-				}
-			}
-			getYearContent()
-			return yearContent
+			setEmployeeId(id)
+			return getYearContent()
 		}
 	}
 }
